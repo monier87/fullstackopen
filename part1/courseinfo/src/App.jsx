ed1a17c574d27833110e3fd8307b1844873629cb
Carlos Monier <carlos.monier872@gmail.com>
2025-02-28 01:27:44 -0500

    1.3

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,36 @@
+const App = () => {
+  const course = 'Half Stack application development';
+  
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
+
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <ul>
+        {parts.map((part, index) => (
+          <li key={index}>
+            {part.name}: {part.exercises} exercises
+          </li>
+        ))}
+      </ul>
+      <p><strong>Total exercises: {totalExercises}</strong></p>
+    </div>
+  );
+};
+
+export default App;
---
+++ part1/courseinfo/src/Total.jsx
@@ -0,0 +1,5 @@
+const Total = ({ totalExercises }) => {
+    return <p>Number of exercises {totalExercises}</p>;
+  };
+  
+  export default Total;
